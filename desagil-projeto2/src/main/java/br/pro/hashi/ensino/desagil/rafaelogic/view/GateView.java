e9ae00a61a4ccc41ba7439a3985adbc0e94cfebc
Guilherme Schoueri <gui.m.s@hotmail.com>
2018-04-16 23:14:22 -0300

    Update GateView.java

--- desagil-projeto2/src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/GateView.java
+++ desagil-projeto2/src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/GateView.java
@@ -16,15 +16,9 @@ import javax.swing.JColorChooser;
 
 import br.pro.hashi.ensino.desagil.rafaelogic.model.*;
 
-// A classe JPanel representa um painel da interface gráfica,
-// onde você pode adicionar componentes como menus e botões.
-// A interface ActionListener é explicada melhor mais abaixo.
 public class GateView extends SimplePanel implements ActionListener, MouseListener {
 
-	// Não é necessário entender esta linha, mas se você estiver curioso
-	// pode ler http://blog.caelum.com.br/entendendo-o-serialversionuid/.
 	private static final long serialVersionUID = 1L;
-
 
 	private Gate gate;
 	
@@ -50,8 +44,6 @@ public class GateView extends SimplePanel implements ActionListener, MouseListen
 
 		outputBox = new JCheckBox();
 
-		// A classe JLabel representa um componente que é simplesmente texto fixo.
-		// https://docs.oracle.com/javase/tutorial/uiswing/components/label.html
 
 		if(inputs.length == 1){
 			add(inputs[0], 10, 54, 75, 25);
@@ -62,21 +54,12 @@ public class GateView extends SimplePanel implements ActionListener, MouseListen
 		}
 		add(outputBox);
 
-		// Estabelece que este subpainel reage ao usuário ativar as
-		// caixas de seleção. Isso só pode ser feito se este subpainel for
-		// do tipo ActionListener, por isso ele implementa essa interface.
 		for (int i = 0; i < inputs.length; i++) {
 			inputs[i].addActionListener(this);
 		}
 		
-
-		// Estabelece que a ultíma caixa de seleção está desativada.
 		outputBox.setEnabled(false);
 
-		// Não podemos esquecer de chamar update na inicialização,
-		// caso contrário a interface só ficará consistente depois
-		// da primeira interação do usuário com as caixas de seleção.
-		// A definição exata do método update é dada logo abaixo.
 		update();
 		
 		
@@ -84,18 +67,10 @@ public class GateView extends SimplePanel implements ActionListener, MouseListen
 		URL url = getClass().getResource(path);
 		image = new ImageIcon(url).getImage();
 		
-		// Toda componente Swing possui este método, que
-		// permite adicionar objetos que reagem a eventos
-		// de mouse que acontecem nela. Ou seja, ao passar
-		// this como parâmetro, estamos pedindo para a
-		// componente reagir aos próprios eventos de mouse.
 		addMouseListener(this);
 
 	}
 
-
-	// Método que liga as entradas dadas com a lógica das portas e
-	// devolve o resultado.
 	private void update() {
 		Source[] sources = new Source[gate.getSize()];
 		
@@ -130,9 +105,6 @@ public class GateView extends SimplePanel implements ActionListener, MouseListen
 
 	}
 
-
-	// Método exigido pela interface ActionListener, que representa
-	// a reação a qualquer interação do usuário no painel.
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		update();
@@ -143,17 +115,13 @@ public class GateView extends SimplePanel implements ActionListener, MouseListen
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 
-		// Descobre em qual posição o clique ocorreu.
 		int x = e.getX();
 		int y = e.getY();
 
-		// Se o clique foi dentro do circulo colorido...
 		if(Math.pow((x-(185+15)), 2) + Math.pow((y-(122+15)), 2) < Math.pow(15, 2)) {
 
-			// ...então abrimos o seletor de cor...
 			color = JColorChooser.showDialog(this, null, color);
 
-			// ...e atualizamos a tela.
 			repaint();
 		}
 	}
@@ -188,23 +156,13 @@ public class GateView extends SimplePanel implements ActionListener, MouseListen
 	@Override
 	public void paintComponent(Graphics g) {
 
-		// Não podemos esquecer desta linha, pois não somos os
-		// únicos responsáveis por desenhar o painel, como era
-		// o caso no Projeto 1. Agora é preciso desenhar também
-		// componentes internas, e isso é feito pela superclasse.
 		super.paintComponent(g);
 
-		// Desenha a imagem passando posição e tamanho.
-		// O último parâmetro pode ser sempre null.
 		g.drawImage(image, 10, 10, heightComp,widthComp, null);
-
 
 		g.setColor(color);
 		g.fillOval(185, 52, 30, 30);
 
-		
-
-		// Evita bugs visuais em alguns sistemas operacionais.
 		getToolkit().sync();
     }
 }
